d088e195e080e7290f5b872268ea24dc4d33ddf7
Shawn Taylor <shawn@mapsandapps.net>
2025-01-09 18:04:06 -0500

    Init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var navImg = document.querySelector('#menu-icon');
+var navList = document.querySelector('.nav-list');
+navImg.onclick = function() {
+    var navImgSrc = navImg.getAttribute('src');
+    if(navImgSrc === 'images/menu-icon.png') {
+      navList.setAttribute ('style', 'display: block');
+      navImg.setAttribute ('src','images/close-icon.png');
+    } else {
+      navList.setAttribute ('style', 'display: none');
+      navImg.setAttribute ('src','images/menu-icon.png');
+    }
+}
