12fd0b8dd10b2791415b5b589c4d831ec2a29da9
Shawn Taylor <shawn@mapsandapps.net>
2025-01-09 20:45:12 -0500

    Make nav more generalizable

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ var navImg = document.querySelector("#menu-icon");
 var navList = document.querySelector(".nav-list");
 navImg.onclick = function () {
   var navImgSrc = navImg.getAttribute("src");
-  if (navImgSrc === "images/menu-icon.png") {
+  var navImgFile = navImgSrc.split("/").pop();
+  if (navImgFile === "menu-icon.png") {
     navList.setAttribute("style", "display: block");
-    navImg.setAttribute("src", "images/close-icon.png");
+    navImg.setAttribute("src", navImgSrc.replace(navImgFile, "close-icon.png"));
   } else {
     navList.setAttribute("style", "display: none");
-    navImg.setAttribute("src", "images/menu-icon.png");
+    navImg.setAttribute("src", navImgSrc.replace(navImgFile, "menu-icon.png"));
   }
 };
