fbc9601837943800aa46a948534112adba13602b
Shawn Taylor <shawn@mapsandapps.net>
2025-01-09 22:12:52 -0500

    Adjust nav margins

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ navImg.onclick = function () {
   var navImgSrc = navImg.getAttribute("src");
   var navImgFile = navImgSrc.split("/").pop();
   if (navImgFile === "menu-icon.png") {
-    navList.setAttribute("style", "display: block");
+    navList.classList.add("visible");
     navImg.setAttribute("src", navImgSrc.replace(navImgFile, "close-icon.png"));
   } else {
-    navList.setAttribute("style", "display: none");
+    navList.classList.remove("visible");
     navImg.setAttribute("src", navImgSrc.replace(navImgFile, "menu-icon.png"));
   }
 };
